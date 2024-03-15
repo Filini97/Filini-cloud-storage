35796a7760649a6e1568710abebd41927d5a57cd
Alex-Filini <Filini97@yandex.ru>
2024-03-15 15:14:38 +0300

    CloudStorageConfig in progress.

--- src/main/java/ru/filini/cloudstorage/config/CloudStorageConfig.java
+++ src/main/java/ru/filini/cloudstorage/config/CloudStorageConfig.java
@@ -1,5 +1,7 @@
 package ru.filini.cloudstorage.config;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import ru.filini.cloudstorage.jwt.JwtAuthenticationEntryPoint;
 import ru.filini.cloudstorage.jwt.JwtRequestFilter;
 import ru.filini.cloudstorage.service.UserService;
@@ -9,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,11 +21,10 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
-
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
-public class CloudStorageConfig {
+public class CloudStorageConfig extends WebSecurityConfigurerAdapter{
     private final UserService userService;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtRequestFilter jwtRequestFilter;
@@ -70,10 +72,28 @@ public class CloudStorageConfig {
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.cors();
+        httpSecurity.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
 
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/login").permitAll();
+
+        httpSecurity
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
